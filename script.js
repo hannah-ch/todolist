b6393a81115b8ee5b7d12e5c6e7350c8217090d0
hannah-developer <hannahchoi.developer@gmail.com>
2020-07-06 13:35:42 +0900

    refactoring: addEventListener() >> onclick()

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
-const list = document.querySelector("ul");
+// const list = document.querySelector("ul");
 const content = document.querySelector(".contents");
 const li = document.querySelector("li");
 const checkbox = document.querySelector(".cb");
-const addButton = document.querySelector(".button");
+// const addButton = document.querySelector(".button");
 const textinput = document.querySelector(".textinput");
+const ul = document.querySelector("ul");
 let completeOnOff = false;
 
-function addComplete(content, checkbox) {
-  if (!content.classList.contains("complete")) {
-    content.classList.add("complete");
-    completeOnOff = true;
-  } else if (content.classList.contains("complete")) {
-    content.classList.remove("complete");
-    completeOnOff = false;
-  }
+const createLi = document.createElement("li");
 
-  if (checkbox.type === "checkbox") {
-    if (completeOnOff) {
-      checkbox.checked = true;
-    } else if (!completeOnOff) {
-      checkbox.checked = false;
+let todos = [];
+
+const eventList = {
+    addTodo: function () {
+        if (!textinput.value) {
+            alert("Type something");
+        } else {
+            todos += textinput.value;
+            textinput.value = "";
+            ul.appendChild(createLi);
+        }
+    },
+    completeTodo: function () {},
+};
+
+// function addTodoLi(){
+// 	ul.innerHTML="";
+// 	for(let i=0;i<)
+// }
+
+function addComplete() {
+    if (!completeOnOff) {
+        completeOnOff = true;
+        checkbox.checked = true;
+        content.classList.add("complete");
+    } else {
+        completeOnOff = false;
+        checkbox.checked = false;
+        content.classList.remove("complete");
     }
-  }
 }
 
-addButton.addEventListener("click", () => {
-  if (!textinput.value) {
-    alert("정보를 입력하세요");
-  } else {
-    return;
-  }
-});
-
-li.addEventListener("click", (e) => {
-  switch (e.target.className) {
-    case "contents":
-      addComplete(e.target, checkbox);
-      break;
-    case "cb":
-      addComplete(content, e.target);
-      break;
-  }
-});
+// li.addEventListener("click", (e) => {
+//     switch (e.target.className) {
+//         case "contents":
+//             addComplete(e.target, checkbox);
+//             break;
+//         case "cb":
+//             addComplete(content, e.target);
+//             break;
+//     }
+// });

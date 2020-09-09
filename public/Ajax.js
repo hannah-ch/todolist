5de8bd83edbd910ab5e698a6acb5c302a18970e2
hannah-developer <hannahchoi.developer@gmail.com>
2020-09-09 11:54:21 +0900

    modified listRender()

--- public/Ajax.js
+++ public/Ajax.js
@@ -4,7 +4,7 @@ class Ajax{
             .then(res => res.json())
             .then(result => listRender(result))
     }
-
+    
     static deleteData(id, listRender){
         fetch(`http://localhost:3000/todo/delete?id=${id}`)
         .then(res => res.json())
--- public/List.js
+++ public/List.js
@@ -1,12 +1,12 @@
+import Todo from './Todo.js'
+
 class List{
     constructor(){
-        this.ul = document.querySelector("ul")
     }
 
     listRender(results){
-        console.log(this)  
-        // this.$ul.innerHTML = results.map(data => new Todo(data.id, data.title, data.completed))
-        // .map(todo => todo.todoRender()).join('')
+        const ul = document.querySelector('ul')
+        ul.innerHTML = results.map(data => new Todo(data.id, data.title, data.completed)).map(todo => todo.todoRender()).join('')
     }
 }
 
